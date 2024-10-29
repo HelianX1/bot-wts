ce68d60044cc4fe9acc75025089ce9a51415b018
HelianX1 <143973877+HelianX1@users.noreply.github.com>
2024-10-29 12:06:19 -0300

    criando filtro para api
    add palavras chaves para para filtrar a api

--- api_consulta.php
+++ api_consulta.php
@@ -1,5 +1,13 @@
 <?php
 $categoria = $_GET['produto'];
+// converter tudo em minusculo
+$categoria = strtolower($categoria);
+// palavra chave para remover
+$palavras =array("motorola ","samsung ","lg ","apple ","asus ","sony ","microsoft ","nokia ","lenovo ","huawei ","xiaomi ","do ","de ","com ","tem a ","tela ","display ","moto ","aro ","preto ","branco ","verde ","normal ","lcd ","touch ","screen ","touchscreen ","original");
+// remover palavras chave for
+foreach($palavras as $palavra){
+    $categoria = str_replace($palavra,"",$categoria);
+}
 include 'coneccao/coneccao.php';
 include 'function.php';
 // consultar produtos
@@ -7,6 +15,6 @@ $result = consulta($coneccao,$categoria);
 if($result){
     echo $result[0]['produto'];
 }else{
-    echo "Nenhum produto encontrado";
+    echo "";
 }
 ?>
